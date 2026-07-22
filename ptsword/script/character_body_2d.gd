37d811cb3a85ef0991ec81566836583ef9f271ad
makoto yuki <youssefmahmoud1523@gmail.com>
2026-07-23 01:11:41 +0300

    movement
    movement

--- ptsword/script/character_body_2d.gd
+++ ptsword/script/character_body_2d.gd
@@ -3,3 +3,26 @@ extends CharacterBody2D
 @export var speed: float = 300.0
 @export var jump_velocity: float = -400.0
 @export var gravity: float = 980.0
+
+func _physics_process(delta: float) -> void:
+	apply_gravity(delta)
+	handel_jump()
+	handle_horizontal_movement(delta)
+	move_and_slide()
+	
+func apply_gravity(delta: float) -> void:
+		if not is_on_floor():
+			velocity.y += gravity * delta
+		else:
+			velocity.y = 0
+
+func handel_jump() -> void:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity 
+
+func handle_horizontal_movement(delta: float) -> void:
+	var direction := Input.get_axis("move_left","move_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed * delta * 5)
