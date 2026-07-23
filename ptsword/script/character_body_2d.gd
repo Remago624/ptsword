dacc1510e088254e9a44e417f02d8474042434d6
Remago624 <rmg6717@gmail.com>
2026-07-23 05:23:06 +0300

    animation

--- ptsword/script/character_body_2d.gd
+++ ptsword/script/character_body_2d.gd
@@ -3,7 +3,14 @@ extends CharacterBody2D
 @export var speed: float = 160.0
 @export var jump_velocity: float = -250.0
 @export var gravity: float = 980.0
-var stat = "walk"
+const IDLE = "idle"
+const WALK = "walk"
+const RUN = "run"
+const JUMP = "jump"
+const FALL = "falling"
+const SWING = "swing"
+var anime_state
+var state = "walk"
 
 @onready var animated_sprite = $AnimatedSprite2D
 
@@ -12,12 +19,13 @@ func _physics_process(delta: float) -> void:
 	handel_jump()
 	handle_horizontal_movement(delta)
 	move_and_slide()
+	update_animation()
 	if Input.is_action_just_pressed("Run") and is_on_floor():
-		speed = 300.0
-		stat = "run"
+		speed = 220.0
+		state = "run"
 	elif Input.is_action_just_released("Run"):
-		speed = 10.0
-		stat = "walk"
+		speed = 160.0
+		state = "walk"
 	
 func apply_gravity(delta: float) -> void:
 		if not is_on_floor():
@@ -28,14 +36,30 @@ func apply_gravity(delta: float) -> void:
 func handel_jump() -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity 
-		animated_sprite.play("jump")
 
 func handle_horizontal_movement(delta: float) -> void:
 	var direction := Input.get_axis("move_left","move_right")
 	if direction:
 		velocity.x = direction * speed
-		animated_sprite.play(stat)
 		animated_sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed * delta * 5)
-		animated_sprite.play("idle")
+func update_animation() -> void:
+	var direction := Input.get_axis("move_left","move_right")
+	#if animated_sprite.animation == "swing" and animated_sprite.is_playing():
+		#return
+	if !is_on_floor():
+		if velocity.y < 0:
+			set_animation(JUMP)
+		else:
+			set_animation(FALL)
+	elif direction:
+		animated_sprite.play(state)
+	else:
+		set_animation(IDLE)
+	if Input.is_action_just_pressed("swing"):
+		set_animation(SWING)
+func set_animation(anim: String):
+	if anime_state != anim:
+		anime_state = anim
+		animated_sprite.play(anim)
